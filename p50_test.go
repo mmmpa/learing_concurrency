20e1bfb8ad6bc3252b70f21325c738e66572f1c6
mmmpa <mmmpa.mmmpa@gmail.com>
2018-08-11 12:44:08 +0900

    updated

--- p50_test.go
+++ p50_test.go
@@ -5,8 +5,32 @@ import (
 	"fmt"
 )
 
+var rectCount = 10000000
+var workers = 4
+
 func TestCompute(t *testing.T) {
-	rectCount := 8
 	fmt.Println(compute(rectCount))
-	fmt.Println(computeC(rectCount, 6))
+	fmt.Println(computeC(rectCount, workers))
+	fmt.Println(computeCC(rectCount, workers))
+}
+
+func BenchmarkCompute(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		compute(rectCount)
+	}
+}
+
+func BenchmarkComputeC(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		computeC(rectCount, workers)
+	}
+}
+
+func BenchmarkComputeCC(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		computeCC(rectCount, workers)
+	}
 }
